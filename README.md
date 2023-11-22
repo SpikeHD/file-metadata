3acd24ca51ded74ba77fd50161a6772790e54c0b
SpikeHD <spikegdofficial@gmail.com>
2023-11-22 08:19:55 -0800

    feat: better error handling, update docs

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # File Metadata
-Baby lib for getting file metadata. Originally written to work for a GameMaker game a friend is creating.
+Tiny baby library for getting file metadata. Originally written to work for a GameMaker game a friend is creating.
+
+# Table of Contents
+* [Installation](#installation)
+  * [Alternative Installation](#alternative-installation)
+* [Documentation](#documentation)
+  * [General Notes](#general-notes)
+  * [`file_creation_date(path)`](#file_creation_datepath)
+  * [`file_modification_date(path)`](#file_modification_datepath)
+  * [`file_access_date(path)`](#file_access_datepath)
+  * [`file_size(path)`](#file_sizepath)
+  * [`file_exists(path)`](#file_existspath)
+  * [`file_is_directory(path)`](#file_is_directorypath)
 
 # Installation
 
@@ -11,6 +23,11 @@ Baby lib for getting file metadata. Originally written to work for a GameMaker g
 You can also use the dynamic library itself by extracting the `.yymps` file as a zip file, and adding the `.dll`/`.dylib`/`.so` file found within to your project manually by following [this guide](https://forum.gamemaker.io/index.php?threads/basic-extension-creation.42662/) (skip the bits about Visual Studio and such).
 
 # Documentation
+
+## General Notes
+
+* If a function encounters an error, it will return -1.0 and log to stderr.
+* Timestamps are in seconds, not milliseconds.
 
 ## `file_creation_date(path)`
 
--- src/lib.rs
+++ src/lib.rs
@@ -4,65 +4,115 @@ pub fn get_file_metadata(path: &str) -> Result<fs::Metadata, std::io::Error> {
   fs::metadata(path)
 }
 
+pub fn log_and_return(e: impl std::fmt::Display) -> f64 {
+  eprintln!("{}", e);
+  -1.0
+}
+
 #[no_mangle]
 pub extern fn file_creation_date(path: *const i8) -> f64 {
   let path = unsafe { std::ffi::CStr::from_ptr(path).to_bytes() };
-  let path = std::str::from_utf8(path).expect("Invalid UTF-8 string for path");
+  let path = match std::str::from_utf8(path) {
+    Ok(v) => v,
+    Err(e) => return log_and_return(e),
+  };
 
-  let metadata = get_file_metadata(path).expect("Failed to get file metadata");
-  let creation_time = metadata.created().expect("Failed to get file creation time");
+  let metadata = match get_file_metadata(path) {
+    Ok(v) => v,
+    Err(e) => return log_and_return(e),
+  };
 
-  creation_time.duration_since(std::time::UNIX_EPOCH).unwrap().as_secs() as f64
+  let creation_time = match metadata.created() {
+    Ok(v) => v,
+    Err(e) => return log_and_return(e),
+  };
+
+  creation_time.duration_since(std::time::UNIX_EPOCH).unwrap_or_default().as_secs() as f64
 }
 
 #[no_mangle]
 pub extern fn file_modification_date(path: *const i8) -> f64 {
   let path = unsafe { std::ffi::CStr::from_ptr(path).to_bytes() };
-  let path = std::str::from_utf8(path).expect("Invalid UTF-8 string for path");
+  let path = match std::str::from_utf8(path) {
+    Ok(v) => v,
+    Err(e) => return log_and_return(e),
+  };
 
-  let metadata = get_file_metadata(path).expect("Failed to get file metadata");
-  let modification_time = metadata.modified().expect("Failed to get file modification time");
+  let metadata = match get_file_metadata(path) {
+    Ok(v) => v,
+    Err(e) => return log_and_return(e),
+  };
 
-  modification_time.duration_since(std::time::UNIX_EPOCH).unwrap().as_secs() as f64
+  let modification_time = match metadata.modified() {
+    Ok(v) => v,
+    Err(e) => return log_and_return(e),
+  };
+
+  modification_time.duration_since(std::time::UNIX_EPOCH).unwrap_or_default().as_secs() as f64
 }
 
 #[no_mangle]
 pub extern fn file_access_date(path: *const i8) -> f64 {
   let path = unsafe { std::ffi::CStr::from_ptr(path).to_bytes() };
-  let path = std::str::from_utf8(path).expect("Invalid UTF-8 string for path");
+  let path = match std::str::from_utf8(path) {
+    Ok(v) => v,
+    Err(e) => return log_and_return(e),
+  };
 
-  let metadata = get_file_metadata(path).expect("Failed to get file metadata");
-  let access_time = metadata.accessed().expect("Failed to get file access time");
+  let metadata = match get_file_metadata(path) {
+    Ok(v) => v,
+    Err(e) => return log_and_return(e),
+  };
 
-  access_time.duration_since(std::time::UNIX_EPOCH).unwrap().as_secs() as f64
+  let access_time = match metadata.accessed() {
+    Ok(v) => v,
+    Err(e) => return log_and_return(e),
+  };
+
+  access_time.duration_since(std::time::UNIX_EPOCH).unwrap_or_default().as_secs() as f64
 }
 
 #[no_mangle]
 pub extern fn file_size(path: *const i8) -> f64 {
   let path = unsafe { std::ffi::CStr::from_ptr(path).to_bytes() };
-  let path = std::str::from_utf8(path).expect("Invalid UTF-8 string for path");
+  let path = match std::str::from_utf8(path) {
+    Ok(v) => v,
+    Err(e) => return log_and_return(e),
+  };
 
-  let metadata = get_file_metadata(path).expect("Failed to get file metadata");
+  let metadata = match get_file_metadata(path) {
+    Ok(v) => v,
+    Err(e) => return log_and_return(e),
+  };
+
   metadata.len() as f64
 }
 
 #[no_mangle]
 pub extern fn file_exists(path: *const i8) -> bool {
   let path = unsafe { std::ffi::CStr::from_ptr(path).to_bytes() };
-  let path = std::str::from_utf8(path).expect("Invalid UTF-8 string for path");
+  let path = match std::str::from_utf8(path) {
+    Ok(v) => v,
+    Err(_) => return false,
+  };
 
   let metadata = get_file_metadata(path);
-  match metadata {
-    Ok(_) => true,
-    Err(_) => false
-  }
+  
+  metadata.is_ok()
 }
 
 #[no_mangle]
 pub extern fn file_is_directory(path: *const i8) -> bool {
   let path = unsafe { std::ffi::CStr::from_ptr(path).to_bytes() };
-  let path = std::str::from_utf8(path).expect("Invalid UTF-8 string for path");
+  let path = match std::str::from_utf8(path) {
+    Ok(v) => v,
+    Err(_) => return false,
+  };
 
-  let metadata = get_file_metadata(path).expect("Failed to get file metadata");
+  let metadata = match get_file_metadata(path) {
+    Ok(v) => v,
+    Err(_) => return false,
+  };
+
   metadata.is_dir()
 }
