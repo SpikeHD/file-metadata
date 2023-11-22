448f5ecb22c0bfc09a3074d15ebdbd8b232e5ebc
SpikeHD <spikegdofficial@gmail.com>
2023-11-21 17:58:31 -0800

    fix: actions package

--- package.ps1
+++ package.ps1
@@ -1,27 +1,27 @@
 $META_PATH=".\ext\extensions\FileMetadata\FileMetadata.yy"
-$LIB_NAME_NO_EXT="FileMetadata"
-$LIB_NAME=$Env:LIB_NAME
+$LIB_FILE_NO_EXT="FileMetadata"
+$LIB_FILE=$Env:LIB_FILE
 
-Write-Host "Packaging $LIB_NAME..."
+Write-Host "Packaging $LIB_FILE..."
 
 # The metadata file is glorified JSON. There is a single instance of the "filename" key, so we can just use regex
-# to set the value to whatever is in the "LIB_NAME" variable
+# to set the value to whatever is in the "LIB_FILE" variable
 Write-Host "Writing to metadata..."
 
-(Get-Content $META_PATH) -replace '\"filename\":\"[^\"]+\"', "`"filename`":`"$LIB_NAME`"" | Out-File -FilePath $META_PATH -Encoding UTF8
+(Get-Content $META_PATH) -replace '\"filename\":\"[^\"]+\"', "`"filename`":`"$LIB_FILE`"" | Out-File -FilePath $META_PATH -Encoding UTF8
 
-Write-Host "Moving .\target\release\$LIB_NAME to .\ext\Extensions\FileMetadata\"
+Write-Host "Moving .\target\release\$LIB_FILE to .\ext\Extensions\FileMetadata\"
 
 # Copy the actual LIB_FILE from target\release\ to the root ext\Extensions\FileMetadata\ folder
-Copy-Item -Path ".\target\release\$LIB_NAME" -Destination ".\ext\Extensions\FileMetadata\"
+Copy-Item -Path ".\target\release\$LIB_FILE" -Destination ".\ext\Extensions\FileMetadata\"
 
 # Compress the ext folder using deflate:fast to filename "FileMetadata.yymps"
-Compress-Archive -Path ".\ext\*" -DestinationPath "$LIB_NAME.zip" -CompressionLevel Fastest
+Compress-Archive -Path ".\ext\*" -DestinationPath "$LIB_FILE.zip" -CompressionLevel Fastest
 
 # Delete existing yymps file if it exists
-Remove-Item -Path "$LIB_NAME_NO_EXT.yymps" -ErrorAction SilentlyContinue
+Remove-Item -Path "$LIB_FILE_NO_EXT.yymps" -ErrorAction SilentlyContinue
 
 # Rename zip to yymps
-Rename-Item -Path "$LIB_NAME.zip" -NewName "$LIB_NAME_NO_EXT.yymps"
+Rename-Item -Path "$LIB_FILE.zip" -NewName "$LIB_FILE_NO_EXT.yymps"
 
 Write-Host "Done!"
--- package.sh
+++ package.sh
@@ -1,13 +1,14 @@
 META_PATH=./ext/extensions/FileMetadata/FileMetadata.yy
+LIB_NAME_NO_EXT=FileMetadata
 
-echo "Packaging ${LIB_NAME}..."
+echo "Packaging ${LIB_FILE}..."
 
 # The metadata file is glorified JSON. There is a single instance of the "filename" key, so we can just use regex
 # to set the value to whatever is in the "LIB_NAME" variable
-sed -i -E "s/\"filename\":\"[^\"]+\"/\"filename\":\"${LIB_NAME}\"/g" ${META_PATH}
+sed -i -E "s/\"filename\":\"[^\"]+\"/\"filename\":\"${LIB_FILE}\"/g" ${META_PATH}
 
 # copy the actual LIB_FILE from target/release/ to the root ext/Extensions/FileMetadata/ folder
 cp ./target/release/${LIB_FILE} ./ext/extensions/FileMetadata/
 
 # Compress the ext folder using deflate:fast to filename "FileMetadata.yymps"
-zip -r -9 -Z deflate:fast ${LIB_NAME}.yymps ./ext/*
+zip -r -9 -Z deflate:fast ${LIB_NAME_NO_EXT}.yymps ./ext/*
