d8e58ad9cc1e81df95692ce7844aeb5f7d513fc3
SpikeHD <spikegdofficial@gmail.com>
2023-11-21 18:24:18 -0800

    fix: package script

--- package.sh
+++ package.sh
@@ -5,7 +5,13 @@ echo "Packaging ${LIB_FILE}..."
 
 # The metadata file is glorified JSON. There is a single instance of the "filename" key, so we can just use regex
 # to set the value to whatever is in the "LIB_NAME" variable
-sed -i '' -r "s%\"filename\":\"[^\"]+\"%\"filename\":\"${LIB_FILE}\"%g" ${META_PATH}
+UNAME=$(uname -s)
+
+if [[ ${UNAME} == "Darwin" ]]; then
+  sed -i '' -E "s%\"filename\":\"[^\"]+\"%\"filename\":\"${LIB_FILE}\"%g" ${META_PATH}  
+else
+  sed -i -r "s%\"filename\":\"[^\"]+\"%\"filename\":\"${LIB_FILE}\"%g" ${META_PATH}
+fi
 
 # copy the actual LIB_FILE from target/release/ to the root ext/Extensions/FileMetadata/ folder
 cp ./target/release/${LIB_FILE} ./ext/extensions/FileMetadata/
